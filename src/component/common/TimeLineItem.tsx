2583cd1e54347137746605f05253ee40919536a4
9Gu-Mo <129805449+9Gu-Mo@users.noreply.github.com>
2026-01-12 11:12:17 +0900

    feat: timeline 레이아웃 수정

--- src/component/common/TimeLineItem.tsx
+++ src/component/common/TimeLineItem.tsx
@@ -17,24 +17,24 @@ export default function TimeLineItem({ timeline }: Props) {
   const ref = useRef<HTMLLIElement>(null);
 
   const isInView = useInView(ref, {
-    margin: '-40% 0px -40% 0px',
+    margin: '0px 0px -40% 0px',
   });
 
   return (
     <>
       <li
         ref={ref}
-        className="timeline-item relative pl-16"
+        className="timeline-item relative"
       >
         <span
-          className={`timeline-item-dot absolute top-[50%] left-[7px] h-3 w-3 transform-[translateY(-50%)] rounded-full transition-colors duration-300 ${isInView ? 'bg-black' : 'bg-gray-400'}`}
+          className={`timeline-item-dot absolute top-[50%] left-[50%] z-20 h-3 w-3 transition-colors duration-300 ${isInView ? 'bg-black' : 'bg-gray-400'}`}
         />
 
         <motion.div
-          initial={{ opacity: 0, y: 40 }}
-          animate={isInView && { opacity: 1, y: 0 }}
+          initial={{ opacity: 0.2, x: 80 }}
+          animate={isInView && { opacity: 1, x: 0 }}
           transition={{ duration: 0.6, ease: 'easeOut' }}
-          className={`rounded-xl border bg-white p-6 break-all text-black shadow-sm transition-colors ${isInView ? 'border-black' : 'border-gray-200'}`}
+          className={`timeline-item-con rounded-xl border bg-white p-6 break-all text-black shadow-sm transition-colors ${isInView ? 'border-black' : 'border-gray-200'}`}
         >
           <span className="text-sm">{timeline.period}</span>
           <h3 className="mt-2 text-lg font-semibold">{timeline.title}</h3>
