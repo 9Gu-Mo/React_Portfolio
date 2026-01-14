7aa6d45db59048b0ab32b65648e78dc5dfd0e4c6
9Gu-Mo <129805449+9Gu-Mo@users.noreply.github.com>
2026-01-14 15:15:57 +0900

    chore: svg 속성 camelcase로 변경 및 아이콘 추가

--- src/component/common/Skill.tsx
+++ src/component/common/Skill.tsx
@@ -1,28 +1,43 @@
 import IconFigma from '@/component/icon/IconFigma';
+import IconGitHub from '@/component/icon/IconGitHub';
 import IconGitLab from '@/component/icon/IconGitLab';
 import IconHtml from '@/component/icon/IconHtml';
 import IconJira from '@/component/icon/IconJira';
 import IconJquery from '@/component/icon/IconJquery';
 import IconJs from '@/component/icon/IconJs';
+import IconNext from '@/component/icon/IconNext';
 import IconPs from '@/component/icon/IconPs';
 import IconReact from '@/component/icon/IconReact';
 import IconSass from '@/component/icon/IconSass';
 import IconSlack from '@/component/icon/IconSlack';
+import IconTs from '@/component/icon/IconTs';
+import IconVue from '@/component/icon/IconVue';
+import IconZustand from '@/component/icon/IconZustand';
 
 export default function Skill() {
   return (
     <>
-      <div className="flex">
+      <div className="flex gap-4">
+        {/* DEV */}
         <IconReact />
+        <IconJs />
+        <IconTs />
+        <IconJquery />
+        <IconNext />
+        <IconVue />
+        <IconZustand />
+
+        {/* PUB */}
         <IconHtml />
         <IconSass />
+
+        {/* TOOL & WORKFLOW */}
         <IconFigma />
         <IconPs />
         <IconJira />
         <IconGitLab />
-        <IconJquery />
+        <IconGitHub />
         <IconSlack />
-        <IconJs />
       </div>
     </>
   );
