3c5f3a0875c8294ed4ed036718e6505140c6d9a3
9Gu-Mo <129805449+9Gu-Mo@users.noreply.github.com>
2026-02-01 13:36:34 +0900

    fix: header state 변경 관련 높이 계산 수정 및 모바일 환경 ux 개선

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,13 @@ import TextTypingCursor from '@/component/common/TextTypingCursor';
 export default function Home() {
   return (
     <>
-      <div className="wrap relative min-h-dvh w-full">
+      <div className="wrap relative min-h-svh w-full">
         <video
           src="/images/bg/bg3.mp4"
           muted
           loop
           autoPlay
-          className="min-h-dvh w-full object-cover"
+          className="min-h-svh w-full object-cover"
         />
         <TextTypingCursor text={`Hi, My Name Is\nWonMo Gu :)\nWelcome My Portfolio`} />
       </div>
