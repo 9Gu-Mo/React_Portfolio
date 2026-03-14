241584ea26a7ce66dd25e2443edecad37d34f2ae
9Gu-Mo <129805449+9Gu-Mo@users.noreply.github.com>
2026-03-14 10:29:13 +0900

    feat: subset font 적용

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -4,39 +4,39 @@ import localFont from 'next/font/local';
 export const pretendard = localFont({
   src: [
     {
-      path: '../../public/fonts/pretendard/Pretendard-Thin.woff2',
+      path: '../../public/fonts/pretendard/Pretendard-Thin.subset.woff2',
       weight: '100',
     },
     {
-      path: '../../public/fonts/pretendard/Pretendard-ExtraLight.woff2',
+      path: '../../public/fonts/pretendard/Pretendard-ExtraLight.subset.woff2',
       weight: '200',
     },
     {
-      path: '../../public/fonts/pretendard/Pretendard-Light.woff2',
+      path: '../../public/fonts/pretendard/Pretendard-Light.subset.woff2',
       weight: '300',
     },
     {
-      path: '../../public/fonts/pretendard/Pretendard-Regular.woff2',
+      path: '../../public/fonts/pretendard/Pretendard-Regular.subset.woff2',
       weight: '400',
     },
     {
-      path: '../../public/fonts/pretendard/Pretendard-Medium.woff2',
+      path: '../../public/fonts/pretendard/Pretendard-Medium.subset.woff2',
       weight: '500',
     },
     {
-      path: '../../public/fonts/pretendard/Pretendard-SemiBold.woff2',
+      path: '../../public/fonts/pretendard/Pretendard-SemiBold.subset.woff2',
       weight: '600',
     },
     {
-      path: '../../public/fonts/pretendard/Pretendard-Bold.woff2',
+      path: '../../public/fonts/pretendard/Pretendard-Bold.subset.woff2',
       weight: '700',
     },
     {
-      path: '../../public/fonts/pretendard/Pretendard-ExtraBold.woff2',
+      path: '../../public/fonts/pretendard/Pretendard-ExtraBold.subset.woff2',
       weight: '800',
     },
     {
-      path: '../../public/fonts/pretendard/Pretendard-Black.woff2',
+      path: '../../public/fonts/pretendard/Pretendard-Black.subset.woff2',
       weight: '900',
     },
   ],
